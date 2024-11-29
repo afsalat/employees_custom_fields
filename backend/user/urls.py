e3fbc2ad6c6101d0c9b24c269572af589a242c3f
AFSAL_AT <afsalat9@gmail.com>
2024-11-29 22:34:18 +0530

    feat - backend : login and register api added

--- backend/user/urls.py
+++ backend/user/urls.py
@@ -1,26 +1,9 @@
 from django.urls import path
+from . import views
 
 
 urlpatterns = [
-    path('',)
+    path('login/', views.Login, name="login"),
+    path('register/', views.Register, name="register")
 ]
 
-@api_view(['POST'])
-def create_product(request):
-    try: 
-        serializer = ProductSerializer(data=request.data)
-        if not serializer.is_valid():
-            return Response(serializer.errors, status=404)
-        
-        variants = request.data.get('variants', [])
-        product = serializer.save()
-        
-        for variant_data in variants:
-            variant = Variant.objects.create(product=product, name=variant_data['name'])
-            for sub_variant in variant_data['options']:
-                SubVariant.objects.create(variant=variant, option=sub_variant, stock=0.00)
-
-        return Response({"message": "successfully added"}, status=200) 
-    except Exception as e:
-        print(traceback.format_exc())
-        return Response({"error": str(e)}, status=500)
--- backend/user/views.py
+++ backend/user/views.py
@@ -1,11 +1,44 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth import authenticate
+from django.contrib.auth.forms import UserCreationForm
 
 
 
 @api_view(['POST'])
 def Login(request):
     try:
-        pass
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            refresh = RefreshToken.for_user(user)
+            return Response({"refresh":str(refresh),
+                            'access': str(refresh.access_token),
+                            'status': 'success'
+                            })
+        else:
+            return Response({"error":"Invalid credentials"}, status=400)
+
     except Exception as e:
-        return Response({"error":str(e), })
+        return Response({"error":str(e)}, status=500)
+    
+
+
+@api_view(['POST'])
+def Register(request):
+    try:
+        user = UserCreationForm(request.POST)
+        if user.is_valid():
+            user.save()
+            return Response({'message':"successfully registered!!!"}, status=200)
+        else:
+            return Response({'message':"Please check the details"}, status=400)
+
+    except Exception as e:
+        return Response({"error": str(e)}, status=500)
+    
+
